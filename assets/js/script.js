082b2e7223b9b19e3f890be03cdf8f8447135d6b
Александр Кузин <neonver.2020@gmail.com>
2024-01-19 20:46:24 +0300

    :wq

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
 const constructionPrev = document.querySelector('#constructionPrev');
 const constructionNext = document.querySelector('#constructionNext');
-const constructionBackground = document.querySelector('.construction__container')
+const constructionBackground = document.querySelector('.construction')
 const constructionBackgrounds = [
-    {
-        src: '../img/bg-image0.jpg'
-    },
-    {
-        src: '../img/bg-image1.jpg'
-    },
-    {
-        src: '../img/bg-image2.jpg'
-    },
-    {
-        src: '../img/bg-image3.jpg'
-    },
+    "bg-image0.jpg",
+    "bg-image1.jpg",
+    "bg-image2.jpg",
+    "bg-image3.jpg",
 ];
 
 let constructionCount = constructionBackgrounds.length;
@@ -30,11 +22,12 @@ function nextconstructionBackgrounds() {
     updateBackground();
 }
 function updateBackground() {
-    constructionBackgrounds.forEach((background, index) => {
-    if (index === constructionIndex) {
-        background.style.url = constructionBackgrounds[constructionIndex];
-    } else {
-        background.style.url = constructionBackgrounds[constructionIndex];
-    }
-});
+    for(index in constructionBackgrounds) {
+        if (index === constructionIndex) {
+            constructionBackground.style.backgroundImage = 'url("'+constructionBackgrounds[constructionIndex]+'")';
+        } else {
+            console.log(constructionBackgrounds[constructionIndex])
+            constructionBackground.style.backgroundImage = 'url("'+constructionBackgrounds[constructionIndex]+'")';
+        }
+    };
 }
